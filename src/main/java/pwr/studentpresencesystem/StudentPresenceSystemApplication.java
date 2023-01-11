6c1579022c63edf1c75e55f720e89ab31b4b59b1
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2023-01-11 14:09:20 +0100

    Init project

---
+++ src/main/java/pwr/studentpresencesystem/StudentPresenceSystemApplication.java
@@ -0,0 +1,13 @@
+package pwr.studentpresencesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPresenceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentPresenceSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pwr/studentpresencesystem/StudentPresenceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pwr.studentpresencesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPresenceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
